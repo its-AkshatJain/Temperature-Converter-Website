0b204b6a71ef684e4813d45b8d1cae8705eff877
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2023-08-28 16:36:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// For storing the value entered by user in variable
 let CelsiusInput=document.querySelector("#Celsius > input");
 
 let FahrenheitInput=document.querySelector("#Fahrenheit  input");
@@ -6,10 +7,12 @@ let KelvinInput= document.querySelector("#Kelvin input");
 
 let button=document.querySelector(".btn button");
 
+// for rounding off the result to two decimal places
 function roundNumber(number){
     return Math.round(number*100)/100;
 }
 
+// for temperature conversions when input in celsius box
 CelsiusInput.addEventListener("input", function(){
    let cTemp= parseFloat(CelsiusInput.value);
    let fTemp= (cTemp * (9/5)) + 32;
@@ -19,6 +22,7 @@ CelsiusInput.addEventListener("input", function(){
    KelvinInput.value=roundNumber(kTemp);
 } )
 
+// for temperature conversions when input in Fahrenheit box
 FahrenheitInput.addEventListener("input", ()=>{
     let fTemp= parseFloat(FahrenheitInput.value);
     let cTemp= (fTemp - 32)*(5/9);
@@ -28,6 +32,7 @@ FahrenheitInput.addEventListener("input", ()=>{
     KelvinInput.value=roundNumber(kTemp);
  } )
 
+// for temperature conversions when input in Kelvin box
  KelvinInput.addEventListener("input",function(){
    let kTemp = parseFloat(KelvinInput.value);
    let cTemp = kTemp - 273.15;
@@ -37,6 +42,7 @@ FahrenheitInput.addEventListener("input", ()=>{
    FahrenheitInput.value=roundNumber(fTemp);
  })
 
+// for clearing the boxes when button is pressed
  button.addEventListener("click",function(){
     CelsiusInput.value="";
     FahrenheitInput.value="";
